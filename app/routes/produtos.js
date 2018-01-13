7c64d7cecb50fe2d883b71c483c157e71f36ab73
Fabricio Kubota <fabriciok@gmail.com>
2018-01-13 11:53:19 -0200

    Recebendo a conexão como argumento

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,9 +2,9 @@ module.exports = function(app){
     app.get('/produtos', function(req, res){
         
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco;
+        var produtosBanco = app.infra.produtosBanco(connection);
 
-       produtosBanco.lista(connection, function(err, results){
+       produtosBanco.lista(function(err, results){
             if(err!=null){
                 console.log(err);
             } else {
@@ -12,5 +12,16 @@ module.exports = function(app){
             }
         });
         connection.end();
+    });
+
+    app.get('produtos/remove', function(){
+
+        var connection = app.infra.connectionFactory;
+        var produtosBanco = app.infra.produtosBanco;
+        var produto = produtosBanco.carrega(connection. id, callback);
+
+        if(produto) {
+            produtosBanco.remove(connection, produto, callback);
+        }
     });
 }
