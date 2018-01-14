1b104780dfc3b4bcf8a0a3fd03a76e1309012fd0
Fabricio Kubota <fabriciok@gmail.com>
2018-01-14 12:43:05 -0200

    Exibindo as mensagens de erro para o usuario

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -22,17 +22,18 @@ module.exports = function(app){
     });
 
     app.get('/produtos/form', function(req, res){
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao:{}});
     });
 
     app.post('/produtos', function(req, res){
         var produto = req.body;
 
-        var validatorTitulo = req.assert('titulo', 'Título é obrigatório');
-        validatorTitulo.notEmpty();
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+
         var erros = req.validationErrors();
         if(erros){
-            res.render('produtos/form');
+            res.render('produtos/form', {errosValidacao: erros});
             return;
         }
 
