e8f0358cee19f7b5d9455c537800cda0234c76b4
Fabricio Kubota <fabriciok@gmail.com>
2018-01-15 23:04:34 -0200

    Testando a listagem de produtos

--- test/produtos.js
+++ test/produtos.js
@@ -1,6 +1,27 @@
-describe('#ProdutosController', function(){
-    it('#listagem json',function(){
-        console.log("teste de verificacao de listagem json");
-    });
+var http = require('http');
 
+describe('ProdutosController', function(done){
+    it('#listagem json',function(){
+       
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        }
+        
+        http.get(configuracoes, function(res){
+            if(res.statusCode == 200){
+                console.log("Status OK");
+            }
+
+            if(res.headers['content-type'] == 'application/json; charset=utf-8'){
+                console.log("Content-type OK");
+            }
+            done();
+        });
+
+    });
 });
